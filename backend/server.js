eb4a56e306af2db157a81808f071a83dfc11e439
NDXFOR <ajaysuhendra03@gmail.com>
2022-06-22 12:28:35 +0700

    commmit 1

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("Hello world")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/api/goals', (req, res) => {
+    res.send('Get Goals')
+})
+
+app.use('/api/goals', require('./routes/goalRoutes.js'))
+app.listen(port, () => console.log(`Server started on port ${port}`))
