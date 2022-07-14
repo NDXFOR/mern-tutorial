a9764a35c0134e4e0b2299cf35a92f5cd7eb7222
NDXFOR <ajaysuhendra03@gmail.com>
2022-07-14 12:44:38 +0700

    commit 6

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -9,7 +9,8 @@ const getGoals = (req, res) => {
 // @access Private
 const setGoal = (req, res) => {
     if (!req.body.text) {
-        res.status(400).json({ message: 'please add a text field' })
+        res.status(400)
+        throw new Error('please add a text field')
     }
 
     res.status(200).json({ message: 'Set Goals' })
