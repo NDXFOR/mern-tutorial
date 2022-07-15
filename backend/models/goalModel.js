d90c970ab200a562462ec33d77bf67b9e5fc5094
NDXFOR <ajaysuhendra03@gmail.com>
2022-07-15 17:35:51 +0700

    User Model

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose')
 const goalSchema = mongoose.Schema(
     {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "Users",
+        },
         Text: {
             type: String,
-            required: [true, 'please add a text value']
+            required: [true, 'please add a text value'],
         },
     },
     {
