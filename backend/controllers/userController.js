f7baef16ff5fcab9a08d40f773f88803d7c70442
NDXFOR <ajaysuhendra03@gmail.com>
2022-07-15 18:28:22 +0700

    Register User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,23 +1,42 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
 // @desc Register user
 // @route POST /api/users
 // @access Public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+    if (!name || !email || !password) {
+        res.status(400)
+        throw Error('Please add all field')
+    }
+
+    //check if user exist
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400)
+        throw Error('User alredy exists')
+    }
+
     res.json({ message: "Register User" })
-}
+})
 
 // @desc Login user
 // @route POST /api/users/login
 // @access Public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
     res.json({ message: "Login User" })
-}
+})
 
 // @desc Get user data
 // @route GET /api/users/me
 // @access Public
-const getMe = (req, res) => {
+const getMe = asyncHandler(async (req, res) => {
     res.json({ message: "User data display" })
-}
+})
 
 module.exports = {
     registerUser,
